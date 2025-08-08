e94b6c3df2535532b45bcb8d8f3afeda95decc34
EmmanuelObi <obiemmy123@gmail.com>
2025-08-08 13:27:46 +0100

    story

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -43,41 +43,82 @@ export default function AboutPage() {
                         faster.
                     </p>
                 </div>
-                <div className="mt-10 flex flex-col items-center justify-between gap-10 md:flex-row">
-                    <div className="max-w-[400px] lg:max-w-[600px]">
-                        <h2 className="text-center text-2xl font-semibold md:text-start md:text-3xl">
-                            Meet Our Founder and CEO — Mobolaji Bamiro
-                        </h2>
-                        <article>
-                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
-                                Mobolaji Bamiro, the visionary behind Consertex,
-                                brings over [X] years of experience in the oil,
-                                gas, and technology sectors. With a strong
-                                background in [engineering/energy
-                                management/data analytics — adjust to suit], he
-                                founded Consertex with the belief that the
-                                future of the energy industry lies in data
-                                powered decision making.
-                            </p>
-                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
-                                Under Mobolaji Bamiro &apos;s leadership,
-                                Consertex has grown from a bold idea into a
-                                trusted partner for leading energy companies.
-                                Passionate about innovation and sustainability,
-                                he champions solutions that not only improve
-                                business performance but also support
-                                responsible resource management.
-                            </p>
-                        </article>
-                    </div>
+                <div className="mt-10 flex flex-col items-center justify-between gap-10">
                     <div className="h-full w-full lg:h-[500px] lg:w-[500px]">
                         <Image
                             src="/profile.png"
                             alt="CEO profile picture"
                             width={100}
                             height={100}
-                            className="h-full w-full object-contain"
+                            className="w-full object-contain"
                         />
+                    </div>
+                    <div className="max-w-6xl">
+                        <h2 className="text-center text-2xl font-semibold md:text-start md:text-3xl">
+                            Meet Our Co-Founder and CEO — MOBOLAJI OKEAYO BAMIRO
+                        </h2>
+                        <article>
+                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
+                                Mobolaji Okeayo BAMIRO (MOB) is a Business
+                                Development Executive with extensive knowledge
+                                across the Oil and Energy space. MOB has a
+                                wealth of over 16 years experience in the Energy
+                                Sector, holding several key roles across the Oil
+                                and Gas value chain. He also has a complementary
+                                stint of over 3 years in the fintech payments
+                                space offering cross border payment solutions
+                                and technology to key Industry players locally
+                                and globally.
+                            </p>
+                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
+                                MOB has led innovation and value creation at
+                                several fintech giants including Flutterwave, a
+                                renowned Unicorn operating Globally in over 50
+                                countries in the capacity of Vice President of
+                                Business Development.
+                            </p>
+
+                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
+                                During his early career, he worked at Dangote
+                                Cement, in the logistics division before
+                                transitioning fully into the Energy ector with
+                                Oando Plc., where he spent over 11 years holding
+                                leadership roles across several SBUs spanning
+                                from Regional Retail Manager, Commercial
+                                Manager, Non-fuel Revenue Manager and Head of
+                                Business Development ASPM Midstream Jetty to
+                                mention a few.
+                            </p>
+                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
+                                In a bid to offer his extensive knowledge and
+                                expertise back to the Energy Sector, Consertex
+                                Integrated Solutions (CIS) was birthed in 2015,
+                                through which he consulted for businesses such
+                                as SLG (Enyo Retail), Eterna Plc, Optima Energy,
+                                AYM Shafa etc.
+                            </p>
+                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
+                                As a key success story, he developed from
+                                scratch the entire business infrastructure and
+                                commercial viability of 3 new SBUs, Non-Fuel
+                                Revenue, LPG and Alternative Energy at Eterna
+                                Plc.
+                            </p>
+                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
+                                Over the years, he has been able to pioneer
+                                several revenue generating initiatives and
+                                improved the profitability of key assets for
+                                major industry operators through resilience and
+                                a consistent knack for innovation.
+                            </p>
+                            <p className="pt-4 text-center text-sm leading-6 md:text-start md:text-lg md:leading-7">
+                                He holds a Bachelor of Science degree in
+                                Economics from the prestigious University of
+                                Ibadan and a Master of Business Administration
+                                (MBA) from the renowned Lagos Business School,
+                                Pan Atlantic University, Lagos Nigeria.
+                            </p>
+                        </article>
                     </div>
                 </div>
             </div>
